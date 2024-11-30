868968eafbc41df4d5778347725eec1714566a94
karthikeyan manimaran <karthikeyan14june@gmail.com>
2024-11-30 22:49:52 +0530

    karthik: updates

--- backend/core/db.js
+++ backend/core/db.js
@@ -1,9 +1,12 @@
-require('dotenv').config({ path: '../../.env' });
+// In development, load .env file (only if it exists)
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '../../.env' });
+}
 
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
+  user: process.env.DB_USER,        // These will be set either from .env in dev or GitHub secrets in prod
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
