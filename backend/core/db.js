d989256307295d615fc4d45ca3e8474ae4aec30b
karthikeyan manimaran <karthikeyan14june@gmail.com>
2024-11-30 23:17:37 +0530

    karthik: updates

--- backend/core/db.js
+++ backend/core/db.js
@@ -1,23 +1,11 @@
-// In development, load .env file (only if it exists)
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '../../.env' });
-}
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,        // These will be set either from .env in dev or GitHub secrets in prod
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+    user: 'karthi209',
+    password: 'Telecaster@1959',
+    host: '144.126.254.165',
+    port: 5432,
+    database: 'onesandzeros'
 });
-
-client.connect()
-  .then(() => console.log('Connected to the database'))
-  .catch(err => {
-    console.error('Database connection failed:', err.stack);
-    process.exit(1);  // Exit the process with a non-zero status if connection fails
-  });
 
 module.exports = pool;
