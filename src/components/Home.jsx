46275f026837a7aaff3d363cbd659e2f68b653be
karthikeyan manimaran <karthikeyan14june@gmail.com>
2024-12-29 15:40:14 +0530

    building new ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,45 +21,47 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="pages-custom">
-      <section class="welcome">
-        <h1 className="test">Welcome, fellow humans!</h1>
-        <a>This is a website dedicated for all my mapping projects and blog posts. This website is still under-construction!</a>
-      </section>
-      <section class="featured">
-        <h2 className="home-head">Featured Post</h2>
-        <article class="post">
-          <h3><a href="blog/1.html">Setting Up a Local Development Server with Node.js</a></h3>
-          <p>Running a Node.js server in WSL2 allows for efficient, VM-like environments right inside your Windows OS...</p>
-          <a href="blog/1.html" class="read-more">Read More</a>
-        </article>
-      </section>
-
-      <section class="recent-posts">
-        <h2 className="home-head">Recent Posts</h2>
-        <div class="posts-grid">
+    <div className="container">
+      <div className="centered-element">
+        <section>
+          <a>Welcome to this..uh..website. I post about how to make your computer work for you and I also make transit maps.</a>
+        </section>
+      </div>
+    <div className="grid-columns">
+      <section class="column column1">
+          <h2 className="home-head">Featured Post</h2>
           <article class="post">
-            <h3><a href="blog/2.html">Setting Up a Local Development Server with Node.js in WSL2</a></h3>
-            <p>A guide to running Node.js servers on WSL2 for efficient development.</p>
+            <h3><a href="blog/1.html">Setting Up a Local Development Server with Node.js</a></h3>
+            <p>Running a Node.js server in WSL2 allows for efficient, VM-like environments right inside your Windows OS...</p>
+            <a href="blog/1.html" class="read-more">Read More</a>
           </article>
-          <article class="post">
-            <h3><a href="blog/3.html">Configuring Docker to Work Across WSL2 and Windows</a></h3>
-            <p>Learn how to integrate Docker across your WSL2 and Windows environments.</p>
-          </article>
-          <article class="post">
-            <h3><a href="blog/4.html">Using Git from WSL2 for Windows Projects</a></h3>
-            <p>Explore how to use Git in WSL2 to manage projects hosted on Windows.</p>
-          </article>
-          <article class="post">
-            <h3><a href="blog/5.html">Running Jupyter Notebooks on WSL2</a></h3>
-            <p>Make Jupyter Notebooks accessible via your browser for a smoother workflow.</p>
-          </article>
-          <article class="post">
-            <h3><a href="/posts/python-web-wsl2.html">Running Python Web Applications on WSL2</a></h3>
-            <p>Set up Python web applications and expose them to your network.</p>
-          </article>
-        </div>
-      </section>
+        </section>
+        <section class="column">
+          <h2 className="home-head">Recent Posts</h2>
+          <div class="posts-grid">
+            <article class="post">
+              <h3><a href="blog/2.html">Setting Up a Local Development Server with Node.js in WSL2</a></h3>
+              <p>A guide to running Node.js servers on WSL2 for efficient development.</p>
+            </article>
+            <article class="post">
+              <h3><a href="blog/3.html">Configuring Docker to Work Across WSL2 and Windows</a></h3>
+              <p>Learn how to integrate Docker across your WSL2 and Windows environments.</p>
+            </article>
+            <article class="post">
+              <h3><a href="blog/4.html">Using Git from WSL2 for Windows Projects</a></h3>
+              <p>Explore how to use Git in WSL2 to manage projects hosted on Windows.</p>
+            </article>
+            <article class="post">
+              <h3><a href="blog/5.html">Running Jupyter Notebooks on WSL2</a></h3>
+              <p>Make Jupyter Notebooks accessible via your browser for a smoother workflow.</p>
+            </article>
+            <article class="post">
+              <h3><a href="/posts/python-web-wsl2.html">Running Python Web Applications on WSL2</a></h3>
+              <p>Set up Python web applications and expose them to your network.</p>
+            </article>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
