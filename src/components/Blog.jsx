7a0f4f042abff9dae0a617dec51fc95205985a26
karthikeyan manimaran <karthikeyan14june@gmail.com>
2024-12-29 18:13:42 +0530

    new work on minimalist ui

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -45,35 +45,37 @@ const BlogPost = () => {
   }, [slug, navigate]);
 
   return (
-    <div className="master-blog">
-      <div className="pages-individual">
-        {loading ? (
-          <div className="loading-placeholder"></div> // Show while loading
-        ) : (
-          <article className="blog">
-            <div className="header-container">
-              <h1 className="blog-header">{post.title}</h1>
-              <a className="blog-meta">Posted by {post.author} on {new Date(post.publicationdate).toLocaleDateString("en-US", { month: 'long', day: 'numeric', year: 'numeric' })}</a>
-            </div>
-            <ReactMarkdown
-              children={post.content}
-              remarkPlugins={[gfm]}
-              components={{
-                code({ node, inline, className, children, ...props }) {
-                  const match = /language-(\w+)/.exec(className || "");
-                  return inline ? (
-                    <code {...props}>{children}</code>
-                  ) : (
-                    <CodeBlock
-                      language={match ? match[1] : ""}
-                      value={String(children).replace(/\n$/, "")}
-                    />
-                  );
-                },
-              }}
-            />
-          </article>
-        )}
+    <div className="container">
+      <div className="master-blog">
+        <div className="pages-individual">
+          {loading ? (
+            <div className="loading-placeholder"></div> // Show while loading
+          ) : (
+            <article className="blog">
+              <div className="header-container">
+                <h1 className="blog-header">{post.title}</h1>
+                <a className="blog-meta">Posted by {post.author} on {new Date(post.publicationdate).toLocaleDateString("en-US", { month: 'long', day: 'numeric', year: 'numeric' })}</a>
+              </div>
+              <ReactMarkdown
+                children={post.content}
+                remarkPlugins={[gfm]}
+                components={{
+                  code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || "");
+                    return inline ? (
+                      <code {...props}>{children}</code>
+                    ) : (
+                      <CodeBlock
+                        language={match ? match[1] : ""}
+                        value={String(children).replace(/\n$/, "")}
+                      />
+                    );
+                  },
+                }}
+              />
+            </article>
+          )}
+        </div>
       </div>
     </div>
   );
@@ -105,37 +107,40 @@ const BlogList = () => {
   };
 
   return (
-    <div className="pages-custom">
-      <div className="search-container">
-        <input 
-            type="text" 
-            value={searchQuery} 
-            onChange={e => setSearchQuery(e.target.value)} 
-            placeholder="Search with title..." 
-            className="search-input"
-        />
-        <button onClick={handleSearch} className="search-button">Search</button>
-      </div>
-      <div className="blogposts">
-        {posts.map((post) => (
-          <article key={post.postid} className="articleclass">
-            <Link
-              to={`/blog/${post.postid}`}
-              aria-label={`Read more about ${post.title}`}
-            >
-              <div><p>{post.catagory}</p></div>
-              <h4>{post.title}</h4>
-              <div className="meta">
-                <p>
-                  Posted on {new Date(post.publicationdate).toLocaleDateString("en-US", { month: 'long', day: 'numeric', year: 'numeric' })}
-                </p>
-              </div>
-              <img src={`${import.meta.env.VITE_API_URL}/images/${post.postid}.jpg`} alt={post.title} />
-            </Link>
-          </article>
-        ))}
+    <div className="container">
+      <div className="holder">
+        <div className="search-container">
+          <input 
+              type="text" 
+              value={searchQuery} 
+              onChange={e => setSearchQuery(e.target.value)} 
+              placeholder="Search with title..." 
+              className="search-input"
+          />
+          <button onClick={handleSearch} className="search-button">Search</button>
+        </div>
+        <div className="blogposts">
+          {posts.map((post) => (
+            <article key={post.postid} className="articleclass">
+              <Link
+                to={`/blog/${post.postid}`}
+                aria-label={`Read more about ${post.title}`}
+              >
+                <div><p>{post.catagory}</p></div>
+                <h4>{post.title}</h4>
+                <div className="meta">
+                  <p>
+                    Posted on {new Date(post.publicationdate).toLocaleDateString("en-US", { month: 'long', day: 'numeric', year: 'numeric' })}
+                  </p>
+                </div>
+                <img src={`${import.meta.env.VITE_API_URL}/images/${post.postid}.jpg`} alt={post.title} />
+              </Link>
+            </article>
+          ))}
+        </div>
       </div>
     </div>
+
   );
 };
 
